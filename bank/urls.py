b3ca8bd4842adfa1036077ae7a7007a8445f060d
Jokūbas Toločka <jokubas.tj@gmail.com>
2023-05-12 19:24:37 +0100

    enabling admin

--- bank/urls.py
+++ bank/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from bank_api import urls as bank_urls
+from django.contrib import admin
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('bank/', include(bank_urls)),
 ]
